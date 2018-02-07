2f063c578fcbb8b1347a1df3cf39a15f9bde8971
João Henrique <joaoh3326@gmail.com>
2018-02-07 15:10:34 -0300

    challenge-29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function(DOM, doc) {
   'use strict';
 
   /*
@@ -36,4 +36,37 @@
   que será nomeado de "app".
   */
 
-})();
+  function app() {
+    return {
+      init: function init() {
+        this.companyInfo();
+      },
+
+      companyInfo: function companyInfo() {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', '/company.json', true);
+        ajax.send();
+        ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
+      },
+
+      getCompanyInfo: function getCompanyInfo() {
+        debugger;
+        if( !app().isReady.call(this) )
+          return;
+
+        var data = JSON.parse( this.responseText );
+        var $companyName = new DOM('[data-js="company-name"]');
+        var $companyPhone = new DOM('[data-js="company-phone"]');
+        $companyName.get()[0].textContent = data.name;
+        $companyPhone.get()[0].textContent = data.phone;
+      },
+
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      }
+    }
+  }
+
+  app().init();
+
+})(window.DOM, document);
